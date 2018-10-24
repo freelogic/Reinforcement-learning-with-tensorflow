09df947c3db44bc9482541c049d518a959c2b5a7
freelogic <275846224@qq.com>
2018-10-24 15:01:21 +0800

    增加一维线性旅行者的exp和指数版本并测试通过，效率的确高；

--- contents/1_command_line_reinforcement_learning/treasure_on_right.py
+++ contents/1_command_line_reinforcement_learning/treasure_on_right.py
@@ -13,13 +13,19 @@ import time
 np.random.seed(2)  # reproducible
 
 
-N_STATES = 6   # the length of the 1 dimensional world
-ACTIONS = ['left', 'right']     # available actions
+N_STATES = 50   # the length of the 1 dimensional world
+END_POS = N_STATES - 1 # 总共有n_states个位置,所以编号从0到n_states-1，结束位置标志是n_states-1
+BEGIN_POS = 0 # 总共有n_states个位置,所以编号从0到n_states-1，开始位置标志是0
+#ACTIONS = ['left', 'right']     # available actions
+ACTIONS = ['l1', 'r1']     # available actions
+ACTIONS_EXP = ['l1', 'r1','l2', 'r2','l4', 'r4','l8', 'r8']     # available actions
 EPSILON = 0.9   # greedy police
 ALPHA = 0.1     # learning rate
 GAMMA = 0.9    # discount factor
-MAX_EPISODES = 13   # maximum episodes
-FRESH_TIME = 0.3    # fresh time for one move
+MAX_EPISODES = 60   # maximum episodes
+FRESH_TIME = 0.01    # fresh time for one move
+#steps_table = [] # save steps used every EPISODE
+display_steps = True # display steps in procedure, it make program VERY SLOW!
 
 
 def build_q_table(n_states, actions):
@@ -31,19 +37,19 @@ def build_q_table(n_states, actions):
     return table
 
 
-def choose_action(state, q_table):
+def choose_action(state, q_table, actions):
     # This is how to choose an action
     state_actions = q_table.iloc[state, :]
     if (np.random.uniform() > EPSILON) or ((state_actions == 0).all()):  # act non-greedy or state-action have no value
-        action_name = np.random.choice(ACTIONS)
+        #action_name = np.random.choice(ACTIONS)
+        action_name = np.random.choice(actions)
     else:   # act greedy
         action_name = state_actions.idxmax()    # replace argmax to idxmax as argmax means a different function in newer version of pandas
     return action_name
 
-
-def get_env_feedback(S, A):
+def get_env_feedback_orig(S, A):
     # This is how agent will interact with the environment
-    if A == 'right':    # move right
+    if A == 'r1':    # move right
         if S == N_STATES - 2:   # terminate
             S_ = 'terminal'
             R = 1
@@ -59,24 +65,53 @@ def get_env_feedback(S, A):
     return S_, R
 
 
+def get_env_feedback(S, A):
+    # This is how agent will interact with the environment
+    if A[0] == 'r': # move right
+        step_nbr = int(A[1:]) # get step number
+        if S + step_nbr > END_POS: # beyond the end pos, new round again! start from begin pos
+            R = 0
+            S_ = BEGIN_POS
+        elif S + step_nbr == END_POS: # reach end pos/goal! success!
+            R = 1
+            S_ = 'terminal'
+        else:  # go right (step_nbr) steps and not reach/beyond end pos yet!
+            R = 0
+            S_ = S + step_nbr
+    elif A[0] == 'l': # move left
+        step_nbr = int(A[1:]) # get step number
+        if S - step_nbr <= BEGIN_POS: # beyond/reach the begin pos/wall, new round again! start from begin pos
+            R = 0
+            S_ = BEGIN_POS
+        else: # go left (step_nbr) steps and not reach/beyond the WALL/BEGIN pos yet!
+            R = 0
+            S_ = S - step_nbr
+    else:
+        print("[WARN]Bad action: '{0}'!".format(A))
+        exit(-1)
+    return S_, R
+
 def update_env(S, episode, step_counter):
     # This is how environment be updated
     env_list = ['-']*(N_STATES-1) + ['T']   # '---------T' our environment
     if S == 'terminal':
         interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
         print('\r{}'.format(interaction), end='')
-        time.sleep(2)
+        time.sleep(FRESH_TIME)
         print('\r                                ', end='')
     else:
-        env_list[S] = 'o'
+        #env_list[S] = 'o'
+        env_list[S] = str(S)
         interaction = ''.join(env_list)
         print('\r{}'.format(interaction), end='')
         time.sleep(FRESH_TIME)
 
 
-def rl():
+def rl(get_env_feedback_func,actions):
     # main part of RL loop
-    q_table = build_q_table(N_STATES, ACTIONS)
+    #q_table = build_q_table(N_STATES, ACTIONS)
+    steps_table = []
+    q_table = build_q_table(N_STATES, actions)
     for episode in range(MAX_EPISODES):
         step_counter = 0
         S = 0
@@ -84,8 +119,9 @@ def rl():
         update_env(S, episode, step_counter)
         while not is_terminated:
 
-            A = choose_action(S, q_table)
-            S_, R = get_env_feedback(S, A)  # take action & get next state and reward
+            A = choose_action(S, q_table,actions)
+            #S_, R = get_env_feedback(S, A)  # take action & get next state and reward
+            S_, R = eval(get_env_feedback_func)(S, A) # dynamic/runtime get func name and handler first before call it!
             q_predict = q_table.loc[S, A]
             if S_ != 'terminal':
                 q_target = R + GAMMA * q_table.iloc[S_, :].max()   # next state is not terminal
@@ -96,12 +132,38 @@ def rl():
             q_table.loc[S, A] += ALPHA * (q_target - q_predict)  # update
             S = S_  # move to next state
 
-            update_env(S, episode, step_counter+1)
+            if display_steps:
+                update_env(S, episode, step_counter+1)
             step_counter += 1
-    return q_table
+        steps_table.append(step_counter) # save steps used
+    return q_table,steps_table
 
 
 if __name__ == "__main__":
-    q_table = rl()
-    print('\r\nQ-table:\n')
-    print(q_table)
+
+    """
+    func_list = [get_env_feedback, get_env_feedback2]
+
+    for func in func_list:
+        func_name = func.__name__
+        print('RUN: {0}'.format(func_name))
+        q_table, steps_table = rl(func_name,ACTIONS)
+        print('\r\nSteps-table: \n{0}'.format(steps_table))
+        print('\r\nQ-table:\n{0}'.format(q_table))
+        print('\r\n') 
+    """
+
+    env_feedback_func = [get_env_feedback]
+    #actions_list = [ACTIONS, ACTIONS_EXP]
+    actions_list = [ACTIONS_EXP]
+
+    for actions in actions_list:
+        print('RUN in actions mode: {0}'.format(actions))
+        q_table, steps_table = rl(env_feedback_func[0].__name__,actions)
+        print('\r\nSteps-table: \n{0}'.format(steps_table))
+        print('\r\nQ-table:\n{0}'.format(q_table))
+        print('\r\n')
+
+# TODO 增加时间的统计；
+# TODO 将steps递减和round弄成二维表；动态比较两种方法的情况；
+# TODO 如果能将时间显示在图表上就更好了；
